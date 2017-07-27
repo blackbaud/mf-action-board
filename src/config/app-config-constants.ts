9167dfa86c2cb9ac1f56306680b1108119c9be6f
Chris Cotar <christopher.cotar@blackbaud.com>
2017-07-26 22:10:58 -0500

    remove sandbox...

--- src/config/app-config-constants.ts
+++ src/config/app-config-constants.ts
@@ -10,6 +10,5 @@ export const JENKINS_ACTION_ITEM_SLA_MS = 43200000;
 export const JENKINS_JOB_BUILDING_COLOR = 'red_anime';
 export const JENKINS_ENV = [
     'https://jenkins-oscf-dev.blackbaudcloud.com/',
-    'https://jenkins-oscf-releases.blackbaudcloud.com/',
-    'https://jenkins-oscf-sandbox.blackbaudcloud.com/'
+    'https://jenkins-oscf-releases.blackbaudcloud.com/'
 ];
