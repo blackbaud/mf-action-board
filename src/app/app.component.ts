6aed8048c7bc4d0f6ed4033b4962a5b74863ce64
Beth Richardson <beth.richardson@blackbaud.com>
2017-08-18 10:26:56 -0500

    Create constants files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { APP_LABELS } from './app.constants';
 
 @Component({
   selector: 'mf-app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Action Item Dashboard';
+  title = APP_LABELS.TITLE;
 }
