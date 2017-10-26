c55635d35964054897efef28375fdfd12b4cecfb
Colby M. White <blackbaud-colbywhite@users.noreply.github.com>
2017-10-26 16:50:28 -0500

    add parent class for action-item comps

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,10 @@
-
 export const APP_LABELS = {
-    TITLE: 'Action Item Dashboard'
+  TITLE: 'Action Item Dashboard'
+};
+
+export const PRIORITY = {
+  RED: 'red',
+  ORANGE: 'orange',
+  YELLOW: 'yellow',
+  GREY: 'grey'
 };
