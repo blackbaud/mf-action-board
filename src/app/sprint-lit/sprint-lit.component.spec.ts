c5c05cc23040626f92d19d7ec6c7fc1f23c73d17
John Nelson <john.nelson@blackbaud.com>
2018-09-04 08:51:26 -0500

    Lum 29800 cycle lit images (#163)
    * RED: Add test for litImage
    
    * GREEN: Add test for litImage
    
    * BLUE: Randomly set litImage
    
    * BLUE: Use polling service to refresh litImage on timer
    
    * BLUE: Extract SPRINT_LIT_REFRESH_INTERVAL_IN_MS as a constant
    
    * BLUE: Add new assets to litImages
    
    * BLUE: Increase SPRINT_LIT_REFRESH_INTERVAL_IN_MS to 15 minutes
    
    * BLUE: Peer review. Remove double negative
    Put lit images in /assets/lit directory

--- src/app/sprint-lit/sprint-lit.component.spec.ts
+++ src/app/sprint-lit/sprint-lit.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SprintLitComponent } from './sprint-lit.component';
+import {PollingService} from '../polling.service';
 
 describe('SprintLitComponent', () => {
   let component: SprintLitComponent;
   let fixture: ComponentFixture<SprintLitComponent>;
+  const doNothingPollingService = {
+    startPoll: (interval: number, func: () => void) => {
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SprintLitComponent ]
+      declarations: [ SprintLitComponent ],
+      providers: [{provide: PollingService, useValue: doNothingPollingService}]
     })
     .compileComponents();
   }));
@@ -21,5 +27,6 @@ describe('SprintLitComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.litImage).toBeDefined();
   });
 });
--- src/app/sprint-lit/sprint-lit.component.ts
+++ src/app/sprint-lit/sprint-lit.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { PollingService } from '../polling.service';
+import { SPRINT_LIT_REFRESH_INTERVAL_IN_MS } from "../app.constants";
 
 @Component({
   selector: 'mf-sprint-lit',
   templateUrl: './sprint-lit.component.html',
   styleUrls: ['./sprint-lit.component.css']
 })
+
 export class SprintLitComponent {
+
+  litImage: string;
+  litImages: Array<string> = [
+    "../assets/lit/ethanLIT.jpg",
+    "../assets/lit/carolineLIT.jpg",
+    "../assets/lit/gregLIT.jpg",
+    "../assets/lit/rajLIT.jpg"
+  ];
+
+  constructor(private pollingService: PollingService) {
+    this.setLitImage();
+    this.pollingService.startPoll(SPRINT_LIT_REFRESH_INTERVAL_IN_MS, this.setLitImage);
+  }
+
+  setLitImage = (): void => {
+    this.litImage = this.litImages[Math.floor(Math.random() * this.litImages.length)];
+  }
 }
