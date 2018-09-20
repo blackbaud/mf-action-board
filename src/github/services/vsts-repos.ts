04005f27f71a72b9a37baa4c0809ea125f3385e5
Colby M. White <Blackbaud-ColbyWhite@users.noreply.github.com>
2018-09-20 09:39:20 -0500

    add missing comma from #172 (#173)
    * add missing comma from #172
    
    * Removing extra comma after social-posting

--- src/github/services/vsts-repos.ts
+++ src/github/services/vsts-repos.ts
@@ -37,8 +37,8 @@ export const VSTS_REPOS = {
     'skyux-spa-social',
     'skyux-spa-social-smoke-tests'
     'social-metrics',
-    'social-metrics-database'
-    'social-posting',
+    'social-metrics-database',
+    'social-posting'
   ],
   'highlander': [
     'forms-management',
