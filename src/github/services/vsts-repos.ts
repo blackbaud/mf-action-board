fec2eb493f356eea27cf07d3d9f6ef005f8fd5f9
Matthew Mazaika <Matthew.Mazaika@blackbaud.com>
2019-08-30 12:50:12 -0500

    add facebook-api-client to illuminati

--- src/github/services/vsts-repos.ts
+++ src/github/services/vsts-repos.ts
@@ -78,6 +78,7 @@ export const VSTS_REPOS = {
     'skyux-spa-receipt-manager'
   ],
   'illuminati' : [
-    'social-donation-receiver'
+    'social-donation-receiver',
+    'facebook-api-client'
   ]
 };
